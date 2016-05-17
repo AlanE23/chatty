ee04d228e10232be01863501484b0565853915bf
Alan Eatherly <eatherlyae@gmail.com>
2016-05-17 14:20:22 -0500

    add files for live page

---
+++ bwMessage.js
@@ -0,0 +1,47 @@
+var Chatty = (function(aug){
+	var messages = [];
+	var updateTime = new Date();
+	var para = document.getElementsByTagName("p");
+
+
+		aug.addNewMessage = function(newItem){
+				var para = document.getElementsByTagName("p");
+				if(newItem === "" || newItem === " "){       //TRY TO FIX MULTIPLE SPACES
+					alert("Text field cannot be empty");
+				}
+				else if (messages.length >= 20) {
+					updateTime = new Date();
+					updateTime = updateTime.toLocaleTimeString() + updateTime.toLocaleDateString();
+					messages.shift(messages[0]);    //HOW CAN YOU USE THE ARRAY TO BUILD THE DOM
+					msgArea.removeChild(para[0]);
+					messages.push(newItem);
+					msgArea.innerHTML
+									  += "<div class='msg'><p class='userMsg'>" +
+									  newItem + "</p>"
+									  " <button type='button' class='edit'>Edit</button>" +
+									  "<button type='button' class='delete'>Delete</button> " + updateTime + "</div>";
+				}
+				else {
+					messages.push(newItem);
+					updateTime = new Date();
+					updateTime = updateTime.toLocaleTimeString() + " " +updateTime.toLocaleDateString();
+					msgArea.innerHTML += "<div class='msg'><p class='userMsg'>" +
+									  newItem + "</p>" +
+									  " " +
+									  updateTime + " <button type='button' class='edit'>Edit</button>" +
+									  " <button type='button' class='delete'>Delete</button></div>";
+				};
+		};
+
+		aug.getMessages = function() {
+			return messages;
+		}
+
+		aug.deleteData = function(ex, span) {
+			messages.splice(ex, span);
+		}
+
+	return aug;
+}(Chatty || {}));
+
+
